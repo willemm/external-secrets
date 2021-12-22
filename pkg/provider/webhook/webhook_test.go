89e6b406dd1fcc7c5f153f75fde3011c02ccfd3c
Willem Monsuwe <w.monsuwe@stater.nl>
2021-12-22 12:23:28 +0100

    Added a bunch of unit tests

--- pkg/provider/webhook/webhook_test.go
+++ pkg/provider/webhook/webhook_test.go
@@ -14,33 +14,35 @@ limitations under the License.
 package webhook
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
-	"io"
 	"strings"
-	"bytes"
 	"testing"
 
 	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	esv1alpha1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1alpha1"
-	// "fmt"
 )
 
 type testCase struct {
-	Case   string `json:"case,omitempty"`
-	Args   args   `json:"args"`
-	Want   want   `json:"want"`
+	Case string `json:"case,omitempty"`
+	Args args   `json:"args"`
+	Want want   `json:"want"`
 }
 
 type args struct {
-	URL      string `json:"url,omitempty"`
-	Key      string `json:"key,omitempty"`
-	Version  string `json:"version,omitempty"`
-	JSONPath string `json:"jsonpath,omitempty"`
-	Response string `json:"response,omitempty"`
+	URL        string `json:"url,omitempty"`
+	Key        string `json:"key,omitempty"`
+	Body       string `json:"body,omitempty"`
+	Version    string `json:"version,omitempty"`
+	JSONPath   string `json:"jsonpath,omitempty"`
+	Response   string `json:"response,omitempty"`
+	StatusCode int    `json:"statuscode,omitempty"`
 }
 
 type want struct {
@@ -50,23 +52,91 @@ type want struct {
 }
 
 var testCases = `
-case: good path
+case: error url
+args:
+  url: /api/getsecret?id={{ .unclosed.template
+want:
+  err: failed to parse url
+---
+case: error body
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  body: Body error {{ .unclosed.template
+want:
+  err: failed to parse body
+---
+case: error connection
+args:
+  url: 1/api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+want:
+  err: failed to call endpoint
+---
+case: error not found
 args:
   url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
   key: testkey
-  version: "1"
+  version: 1
+  statuscode: 404
+  response: not found
+want:
+  path: /api/getsecret?id=testkey&version=1
+  err: endpoint gave error 404
+---
+case: error bad json
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  key: testkey
+  version: 1
+  jsonpath: $.result.thesecret
+  response: '{"result":{"thesecret":"secret-value"}'
+want:
+  path: /api/getsecret?id=testkey&version=1
+  err: failed to parse response json
+---
+case: error bad jsonpath
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  key: testkey
+  version: 1
+  jsonpath: $.result.thesecret
+  response: '{"result":{"nosecret":"secret-value"}}'
+want:
+  path: /api/getsecret?id=testkey&version=1
+  err: failed to get response path
+---
+case: error bad json data
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  key: testkey
+  version: 1
+  jsonpath: $.result.thesecret
+  response: '{"result":{"thesecret":{"one":"secret-value"}}}'
+want:
+  path: /api/getsecret?id=testkey&version=1
+  err: failed to get response (wrong type)
+---
+case: good plaintext
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  key: testkey
+  version: 1
+  response: secret-value
+want:
+  path: /api/getsecret?id=testkey&version=1
+  err: ""
+  result: secret-value
+---
+case: good json
+args:
+  url: /api/getsecret?id={{ .remoteRef.key }}&version={{ .remoteRef.version }}
+  key: testkey
+  version: 1
   jsonpath: $.result.thesecret
   response: '{"result":{"thesecret":"secret-value"}}'
 want:
   path: /api/getsecret?id=testkey&version=1
   err: ""
   result: secret-value
----
-case: url error
-args:
-  url: /api/getsecret?id={{ .unclosed.template
-want:
-  err: failed to parse url
 `
 
 func TestWebhookGetSecret(t *testing.T) {
@@ -74,28 +144,29 @@ func TestWebhookGetSecret(t *testing.T) {
 	for {
 		var tc testCase
 		if err := ydec.Decode(&tc); err != nil {
-			if err != io.EOF {
+			if !errors.Is(err, io.EOF) {
 				t.Errorf("testcase decode error %w", err)
 			}
 			break
 		}
-		// fmt.Printf("Running testcase %s\n", tc.Case)
-		runTestCase(&tc, t)
+		runTestCase(tc, t)
 	}
 }
 
-func runTestCase(tc *testCase, t *testing.T) {
-	// fmt.Printf("Testcase %s:\n  url '%s' -> path '%s'\n  key '%s' version '%s' -> err '%s' + result '%s'\n  response '%s' -> jsonpath '%s'\n", tc.Case, tc.Args.URL, tc.Want.Path, tc.Args.Key, tc.Args.Version, tc.Want.Err, tc.Want.Result, tc.Args.Response, tc.Args.JSONPath)
+func runTestCase(tc testCase, t *testing.T) {
 	// Start a new server for every test case because the server wants to check the expected api path
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		if tc.Want.Path != "" && req.URL.String() != tc.Want.Path {
 			t.Errorf("%s: unexpected api path: %s, expected %s", tc.Case, req.URL.String(), tc.Want.Path)
 		}
+		if tc.Args.StatusCode != 0 {
+			rw.WriteHeader(tc.Args.StatusCode)
+		}
 		rw.Write([]byte(tc.Args.Response))
 	}))
 	defer ts.Close()
 
-	testStore := makeClusterSecretStore(ts.URL+tc.Args.URL, tc.Args.JSONPath)
+	testStore := makeClusterSecretStore(ts.URL, tc.Args)
 	testProv := &Provider{}
 	client, err := testProv.NewClient(context.Background(), testStore, nil, "testnamespace")
 	if err != nil {
@@ -108,25 +179,19 @@ func runTestCase(tc *testCase, t *testing.T) {
 		Version: tc.Args.Version,
 	}
 	secret, err := client.GetSecret(context.Background(), testRef)
-	if !errorContains(err, tc.Want.Err) {
-		t.Errorf("%s: unexpected error: %s (expected '%s')", tc.Case, err.Error(), tc.Want.Err)
+	errStr := ""
+	if err != nil {
+		errStr = err.Error()
+	}
+	if !strings.Contains(errStr, tc.Want.Err) {
+		t.Errorf("%s: unexpected error: '%s' (expected '%s')", tc.Case, errStr, tc.Want.Err)
 	}
 	if err == nil && string(secret) != tc.Want.Result {
-		t.Errorf("%s: unexpected response: %s (expected '%s')", tc.Case, secret, tc.Want.Result)
+		t.Errorf("%s: unexpected response: '%s' (expected '%s')", tc.Case, secret, tc.Want.Result)
 	}
 }
 
-func errorContains(out error, want string) bool {
-	if out == nil {
-		return want == ""
-	}
-	if want == "" {
-		return false
-	}
-	return strings.Contains(out.Error(), want)
-}
-
-func makeClusterSecretStore(url, jsonpath string) *esv1alpha1.ClusterSecretStore {
+func makeClusterSecretStore(url string, args args) *esv1alpha1.ClusterSecretStore {
 	return &esv1alpha1.ClusterSecretStore{
 		TypeMeta: metav1.TypeMeta{
 			Kind: "ClusterSecretStore",
@@ -138,13 +203,14 @@ func makeClusterSecretStore(url, jsonpath string) *esv1alpha1.ClusterSecretStore
 		Spec: esv1alpha1.SecretStoreSpec{
 			Provider: &esv1alpha1.SecretStoreProvider{
 				Webhook: &esv1alpha1.WebhookProvider{
-					URL: url,
+					URL:  url + args.URL,
+					Body: args.Body,
 					Headers: map[string]string{
 						"Content-Type": "application.json",
 						"X-SecretKey":  "{{ .remoteRef.key }}",
 					},
 					Result: esv1alpha1.WebhookResult{
-						JSONPath: jsonpath,
+						JSONPath: args.JSONPath,
 					},
 				},
 			},
